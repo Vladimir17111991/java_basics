8145bb6377414196ea8fa0c58bc6d01ceb084dcd
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-01 19:22:43 +0300

    homework 7.4 done

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,23 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneOffset;
 
 public class Main {
 
   public static void main(String[] args) {
-
+int day = 23;
+int month = 5;
+int year = 1995;
+LocalDate birthday = LocalDate.of(year,month,day);
+      System.out.println(getPeriodFromBirthday(birthday));
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+LocalDate now = LocalDate.now();
+      String text;
+      Period period = Period.between(birthday,now);
+      text = period.getYears() + " years, " + period.getMonths() + " month, " +period.getDays() + " days, ";
+      return text ;
   }
-
 }
