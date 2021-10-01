e2a4c954c3352f1bcbd65ecb5f23139b793425ab
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-01 18:56:56 +0300

    homework 7.3 done

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,22 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+          int day = 31;
+          int month = 12;
+          int year = 1990;
 
         System.out.println(collectBirthdays(year, month, day));
-
     }
-
     public static String collectBirthdays(int year, int month, int day) {
-
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        String text = "";
+        int g = 0;
+            while(today.compareTo(birthday)>=0)  {
+                birthday=birthday.plusYears(g);
+                if(birthday.isBefore(today)||(birthday.isEqual(today)))
+                {
+                text =text+ ((g) + " - " + formatter.format(birthday) + System.lineSeparator());
+                g++;
+                }
+            }
+        return text;
     }
+    //TODO реализуйте метод для построения строки в следующем виде
+    //0 - 31.12.1990 - Mon
+    //1 - 31.12.1991 - Tue
 }
