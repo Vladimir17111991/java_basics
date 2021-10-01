684a3a5286c6887322c11c896049cdc333a4324c
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-01 09:01:27 +0300

    homework 7.1 done

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,32 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+
+        return Math.PI*Math.pow(radius,2);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+
+        return Math.abs(4 * Math.PI * Math.pow(radius,3) / 3);
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
-    }
+        boolean abC = a + b > c;
+        boolean acB = a + c > b;
+        boolean bcA = b + c > a;
+            return (abC && acB && bcA);
+        }
+
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        double p = (a + b + c)/2;
+        if(isTrianglePossible(a,b,c))
+        {
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return -1;
     }
 }
