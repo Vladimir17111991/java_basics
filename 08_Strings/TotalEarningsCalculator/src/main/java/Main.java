4beb97eb47236d2e532eca7f5898788c7990daa4
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-04 10:52:56 +0300

    homework 8.1 done.Method summa editing

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,27 +3,24 @@ public class Main {
   public static void main(String[] args) {
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+
     System.out.println(summa(text));
   }
   public static int summa(String text) {
-    int rezult;
-    String text1 = "заработал ";
-    String text2 = " - ";
-
-    int index1 = text.indexOf(text1) + text1.length();
-    int index2 = text.indexOf(" рублей,");
-
-    int index3= text.indexOf(text2) + text2.length();
-    int index4= text.indexOf(" рубля,");
-
-    int index5= text.lastIndexOf(text2) + text2.length();
-    int index6= text.lastIndexOf(" рублей");
-
-    String firstPrice = text.substring(index1,index2).trim();
-    String secondPrice = text.substring(index3,index4).trim();
-    String thirdPrice = text.substring(index5,index6).trim();
-    rezult = Integer.parseInt(firstPrice) + Integer.parseInt(secondPrice) + Integer.parseInt(thirdPrice);
-
+    int rezult = 0;
+    char symbol;
+    String s="";
+for(int i = 0;i<text.length();i++) {
+  symbol = text.charAt(i);
+       if (Character.isDigit(symbol)) {
+      s += symbol;
+         if(text.charAt(i+1)==' ')
+         {
+           rezult +=Integer.parseInt(s);
+           s="";
+         }
+    }
+  }
     return rezult;
     }
 }
