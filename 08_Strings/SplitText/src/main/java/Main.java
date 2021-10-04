7a5bd01654bf04eaa23b0d6499fb8c394a601dbf
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-04 13:53:44 +0300

    homework 8.4 done

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-
+      Scanner scanner = new Scanner(System.in);
+      String letter = scanner.nextLine();
+    System.out.println(splitTextIntoWords(letter));
   }
-
-  public static String splitTextIntoWords(String text) {
     //TODO реализуйте метод
-    return "";
+  public static String splitTextIntoWords(String text) {
+      String regex = "\\s+";
+      String regex2 = "[,-;.0-9]";
+      String modifyText =  text.replaceAll(regex2," ");
+      String rezult = "";
+      String[] massivStr = modifyText.split(regex);
+      for(int i = 0; i < massivStr.length; i++)
+      {
+          if(i==massivStr.length-1) {
+              rezult += massivStr[i];
+          }
+          else {
+              rezult += massivStr[i] + "\n";
+          }
+      }
+      return rezult;
   }
-
 }
