3623b0ad1e0c08cebc8609403b9789ebd4f4eba6
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-04 12:54:28 +0300

    homework 8.3 done

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,37 @@
+import net.sf.saxon.functions.Matches;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      String input = scanner.nextLine();
+      if (input.equals("0")) {
+        break;
+      }
 
+      System.out.println(calculateSalarySum(input));
+    }
   }
-
   public static int calculateSalarySum(String text){
+String regex="[0-9]+";
+String te = "";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    int summa = 0;
+    while (matcher.find())
+    {
+      int start = matcher.start();
+      int end = matcher.end();
+      te =text.substring(start,end);
+      summa += Integer.parseInt(te);
+    }
     //TODO: реализуйте метод
-    return -1;
+    return summa;
   }
 
 }
