517894f0affec0ea59a50c1cfb24d1bcbdc12de5
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-09 13:05:31 +0300

    homework 9.3 done

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,35 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    Set<String> treeSet = new TreeSet<>();
+    List<String> list = new ArrayList<>();
+    String regexMail = "[a-z]+\\d*?@[a-z]+[.][a-z]+";
+
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        Pattern pattern = Pattern.compile(regexMail);
+        Matcher matcher = pattern.matcher(email.trim());
+        if (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            email = email.toLowerCase(Locale.ROOT).substring(start, end).trim();
+            treeSet.add(email);
+            System.out.println("Добавлен email: " + email);
+        } else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        list.addAll(treeSet);
+        for (String s : list) {
+            System.out.println(s);
+        }
+        return list;
     }
 
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -16,18 +18,34 @@ public class Main {
         Пример вывода сообщения об ошибке при неверном формате Email:
         "Неверный формат email"
     */
+   public static EmailList emailList = new EmailList();
 
     public static void main(String[] args) {
+        String regexCommand = "[A-Z]{3,4}";
         Scanner scanner = new Scanner(System.in);
-        
         while (true) {
             String input = scanner.nextLine();
+            Pattern pattern = Pattern.compile(regexCommand);
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                int start = matcher.start();
+                int end = matcher.end();
+                String firstWord = input.substring(start, end).trim();
+                if (firstWord.equals("ADD")) {
+                    input = input.replaceFirst(firstWord, "").trim();
+                    emailList.add(input);
+                }
+                if (firstWord.equals("LIST")) {
+                    emailList.getSortedEmails();
+                }
+            } else {
+                System.out.println("Неверная команда");
+            }
+
             if (input.equals("0")) {
                 break;
             }
-            
             //TODO: write code here
-            
         }
     }
 }
