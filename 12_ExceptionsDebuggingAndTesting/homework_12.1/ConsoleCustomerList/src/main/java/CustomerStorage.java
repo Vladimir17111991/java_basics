ceb2fc5357d83cd587e72478693f5e08c916c564
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-21 10:47:45 +0300

    homework 12.1 done

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -13,10 +15,34 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
-
+        String regexForPhone = "[+\\d]+";
+        String regexForMail = "[a-z]+\\d*?@[a-z]+[.][a-z]+";
         String[] components = data.split("\\s+");
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Correct format: name surname email phone");
+        }
+        if (checkingArrayElements(components, 3, regexForPhone)) {
+            throw new NumberFormatException("Example correct format phone: +79123456789");
+        }
+        if (checkingArrayElements(components, 2, regexForMail)) {
+            throw new IllegalArgumentException("Example correct format e-mail: examples@gmail.com");
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+    }
+
+    public boolean checkingArrayElements(String[] array, int indexArray, String regularExpression) {
+        String result = "";
+        Pattern pattern = Pattern.compile(regularExpression);
+        Matcher matcher = pattern.matcher(array[indexArray]);
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            result = array[indexArray].substring(start, end).trim();
+        }
+        return result.compareTo(array[indexArray]) != 0;
     }
 
     public void listCustomers() {
--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
-            "\tlist\n\tcount\n\tremove Василий Петров";
+            "\tlist\n\tcount\n\tremove Василий Петров\n\t0 - выход из программы";
     private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n" +
             COMMAND_EXAMPLES;
     private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
@@ -14,21 +14,29 @@ public class Main {
         CustomerStorage executor = new CustomerStorage();
 
         while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    if (executor.getCount() == 0) {
+                        throw new NullPointerException("Список пуст");  //доп. задание
+                    }
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else if (tokens[0].equals("0")) {
+                    break;
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            } catch (RuntimeException exception) {
+                exception.printStackTrace();
             }
         }
     }
