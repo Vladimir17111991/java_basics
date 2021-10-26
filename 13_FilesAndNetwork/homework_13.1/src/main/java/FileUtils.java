d6389d23e6f779ea9dff955ca2bb432b44c0c22e
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-26 10:19:06 +0300

    homework 13.1 done

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,33 @@
+import java.io.File;
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long countBytes = 0;
+        try {
+            File folder = new File(path);
+            File[] files = folder.listFiles();
+            assert files != null;
+            for (File file : files) {
+                String pathFile = file.getAbsolutePath();
+                if (file.isDirectory()) {
+                    File subFolder = new File(pathFile);
+                    File[] filesSubFolder = subFolder.listFiles();
+                    assert filesSubFolder != null;
+                    for (File fileNew : filesSubFolder) {
+                        String pathFileInSubFolder = fileNew.getAbsolutePath();
+                        System.out.println(pathFileInSubFolder + " - " + fileNew.length() + " байт");
+                        countBytes += fileNew.length();
+                    }
+                    pathFile = "";
+                }
+                if (!pathFile.equals("")) {
+                    System.out.println(pathFile + " - " + file.length() + " байт");
+                    countBytes += file.length();
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return countBytes;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь до папки: ");
+        String line = scanner.nextLine();
+        long sum = FileUtils.calculateFolderSize(line);
+        System.out.println("Общий размер файлов: ");
+        System.out.println(sum + " байт \n"
+                + sum / (int)Math.round(Math.pow(1024,1)) + " Кб \n"
+                + String.format("%.2f",(double) sum / Math.round(Math.pow(1024,2))) + " Мб \n"
+                + String.format("%.2f",(double) sum / Math.round(Math.pow(1024,3))) + " Гб");
     }
 }
