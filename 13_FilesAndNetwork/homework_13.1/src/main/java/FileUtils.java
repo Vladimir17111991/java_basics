44d973881c1d7f7389550b0a9bc6211f90db0340
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-26 18:11:27 +0300

    homework 13.1 done. Modify. Delete assert.

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -6,13 +6,11 @@ public class FileUtils {
         try {
             File folder = new File(path);
             File[] files = folder.listFiles();
-            assert files != null;
             for (File file : files) {
                 String pathFile = file.getAbsolutePath();
                 if (file.isDirectory()) {
                     File subFolder = new File(pathFile);
                     File[] filesSubFolder = subFolder.listFiles();
-                    assert filesSubFolder != null;
                     for (File fileNew : filesSubFolder) {
                         String pathFileInSubFolder = fileNew.getAbsolutePath();
                         System.out.println(pathFileInSubFolder + " - " + fileNew.length() + " байт");
