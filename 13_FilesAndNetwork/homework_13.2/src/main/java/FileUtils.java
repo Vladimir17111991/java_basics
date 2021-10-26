728668075317350f5aeeda9ad89948e2c5ed6de5
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-26 18:16:41 +0300

    homework 13.2

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,25 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
-    }
+    try {
+            //FileInputStream is = new FileInputStream(sourceDirectory);
+            // FileOutputStream os = new FileOutputStream(destinationDirectory);
+            Files.walk(Paths.get(sourceDirectory)).forEach(source -> {
+                Path os = Paths.get(destinationDirectory, source.toString()
+                        .substring(sourceDirectory.length()));
+                try {
+                    Files.copy(source, os);
+                }
+                catch (IOException ex)
+                {ex.printStackTrace();}
+
+            });
+        }
+    catch (Exception e)
+    {e.printStackTrace();}
+        }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к папке,с которой хотите скопировать");
+        String line = scanner.nextLine();
+       // String s1 = "C:\\mmm\\";
+        System.out.println("Введите путь к папке,в которую хотите скопировать");
+        String line2 = scanner.nextLine();
+       // String s2 = "C:\\mmm2\\";
+        FileUtils.copyFolder(line, line2);
+        //FileUtils.copyFolder(s1, s2);
     }
 }
