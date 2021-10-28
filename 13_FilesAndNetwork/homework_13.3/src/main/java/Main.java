7b2cc1ab0dfa715a07b521ea6817fd3c3e0bad8d
Vladimir <vovchikzaharov1991@gmail.com>
2021-10-28 14:44:37 +0300

    homework 13.3 done

--- 13_FilesAndNetwork/homework_13.3/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.3/src/main/java/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    private static final String DATA_FILE = "src/test/resources/movementList.csv";
     public static void main(String[] args) {
+
+        Movements movements = new Movements(DATA_FILE);
+        System.out.println("Общий расход составил : " + movements.getExpenseSum());
+        System.out.println("Общий доход составил : " + movements.getIncomeSum()+"\n");
+        System.out.println("Суммарный расход по организациям: ");
+        Movements.associationOrganizations(Movements.parseList());
     }
 }
--- 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
+++ 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
@@ -1,13 +1,106 @@
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Movements {
+    private static String pathMovementsCsv;
+    private String organization;
+    private double income;
+    private double expense;
 
     public Movements(String pathMovementsCsv) {
+        this.pathMovementsCsv = pathMovementsCsv;
     }
 
+    public Movements(String organization, double income, double expense) {
+        this.organization = organization;
+        this.income = income;
+        this.expense = expense;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public double getExpense() {
+        return expense;
+    }
+
+    public static List<Movements> parseList() {
+        List<Movements> movementsList = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(pathMovementsCsv));
+            lines.remove(0);
+            for (String line : lines) {
+                String[] fragment = line.split(",(?!\\d+\\042)");
+                if (fragment.length != 8) {
+                    System.out.println("Не корректная линия: " + line + "\n");
+                    continue;
+                }
+                String regexDate = "\\s\\d{2}.\\d{2}.\\d{2}\\s";
+                Pattern pattern = Pattern.compile(regexDate);
+                Matcher matcher = pattern.matcher(fragment[5]);
+                if (matcher.find()) {
+                    int start = matcher.start();
+                    fragment[5] = fragment[5].substring(16, start).trim();
+                }
+
+                movementsList.add(new Movements(fragment[5],
+                        Double.parseDouble(fragment[6]
+                                .replaceAll("\"", "")
+                                .replaceAll(",", ".")),
+                        Double.parseDouble(fragment[7]
+                                .replaceAll("\"", "")
+                                .replaceAll(",", "."))));
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return movementsList;
+    }
+
+    public static Map<String, Double> associationOrganizations(List<Movements> list) {
+        Map<String, Double> mapOrganizations = list.stream()
+                .collect(Collectors.groupingBy(Movements::getOrganization,
+                        Collectors.summingDouble(Movements::getExpense)));
+        for (Map.Entry<String, Double> item : mapOrganizations.entrySet()) {
+            System.out.println(item.getKey() + " - " + item.getValue() + " руб.");
+        }
+        return mapOrganizations;
+    }
     public double getExpenseSum() {
-        return 0.0;
+        double sumExpense = 0;
+        List<Movements> list = parseList();
+        Map<String, Double> mapOrganizations = list.stream()
+                .collect(Collectors.groupingBy(Movements::getOrganization,
+                        Collectors.summingDouble(Movements::getExpense)));
+        for (Map.Entry<String, Double> item : mapOrganizations.entrySet()) {
+            sumExpense += item.getValue();
+        }
+        return sumExpense;
+
     }
 
     public double getIncomeSum() {
-        return 0.0;
+        double sumIncome = 0;
+        List<Movements> list = parseList();
+        Map<String, Double> mapOrganizations = list.stream()
+                .collect(Collectors.groupingBy(Movements::getOrganization,
+                        Collectors.summingDouble(Movements::getIncome)));
+        for (Map.Entry<String, Double> item : mapOrganizations.entrySet()) {
+            sumIncome += item.getValue();
+        }
+        return sumIncome;
     }
 }
