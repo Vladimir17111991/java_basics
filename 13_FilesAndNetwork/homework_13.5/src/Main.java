c89ef7df30e68c5d59f7bb73bdfeb1037dbd37af
Vladimir <vovchikzaharov1991@gmail.com>
2021-11-01 13:53:09 +0300

    homework_13.5. Write from MetroMoscow.html to metroParse.json. Read info on file metroParse.json

--- 13_FilesAndNetwork/homework_13.5/src/Main.java
+++ 13_FilesAndNetwork/homework_13.5/src/Main.java
@@ -5,18 +5,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
-
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.FileReader;
 import java.io.PrintWriter;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class Main {
@@ -27,13 +21,14 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            JSONParser parser = new JSONParser();
             // Document document = Jsoup.connect("https://www.moscowmap.ru/metro.html#lines").maxBodySize(0).get();
             String htmlFile = parseFile("data/MetroMoscow.html");
             Document document = Jsoup.parse(htmlFile);
             downloadDataOnHtml(document);
+            readOnJsonFile(DATA_GSON);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e){e.printStackTrace();}
     }
 
     public static void writeJsonSimpleLine(String path) {
@@ -61,7 +56,7 @@ public class Main {
         return builder.toString();
     }
 
-    public static void downloadDataOnHtml(Document document){
+    public static void downloadDataOnHtml(Document document) {
         Elements lines = document.select("div > div > span.js-metro-line");
         Elements stations = document.select("div > div > div.js-metro-stations");
         lines.forEach(element -> {
@@ -79,9 +74,27 @@ public class Main {
             });
         });
         writeJsonSimpleLine(DATA_GSON);
+    }
 
-}
-
+    public static void readOnJsonFile(String pathOnJsonFile) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject object = (JSONObject) parser.parse(new FileReader(pathOnJsonFile));
+            JSONObject objectLine = (JSONObject) object.get("stations");
+            HashMap<String, Object> map = new HashMap<>();
+            Iterator<String> iterator = objectLine.keySet().iterator();
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                JSONArray arrayStationOnLine = (JSONArray) objectLine.get(key);
+                map.put(key, arrayStationOnLine.size());
+            }
+            for (Map.Entry<String, Object> item : map.entrySet()) {
+                System.out.println("Линия - " + item.getKey() + ". Колличество станций - " + item.getValue());
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 }
 
 
